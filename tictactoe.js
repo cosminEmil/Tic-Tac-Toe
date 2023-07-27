19f84eb99a9dedb2982e7478a6ca520392e1fe21
cosminEmil <132258074+cosminEmil@users.noreply.github.com>
2023-07-27 13:23:11 +0300

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -18,53 +18,25 @@ function addValue(button) {
 
 function checkButtons(button1, button2, button3) {
     if (button1.innerText == "X" && button2.innerText == button1.innerText && button1.innerText == button3.innerText) {
-        return 1;
-    }
-    if (button1.innerText == "O" && button2.innerText == button1.innerText && button1.innerText == button3.innerText) {
-        return 2;
+        alert("Player 1 Wins");
+        window.location.reload();
+    } else if (button1.innerText == "O" && button2.innerText == button1.innerText && button1.innerText == button3.innerText) {
+        alert("Player 2 Wins");
+        window.location.reload();
     }
     return 0;
 }
 
 function checkWinner() {
-    let xWinner = 0;
-    let oWinner = 0;
     for (let i = 0; i < 7; i += 3) {
         let check = checkButtons(buttons[i], buttons[i + 1], buttons[i + 2]);
-        if (check == 1) {
-            ++xWinner;
-        } else if (check == 2) {
-            ++oWinner;
-        }
     }
-
     for (let i = 0; i < 3; ++i) {
         let check = checkButtons(buttons[i], buttons[i + 3], buttons[i + 6]);
-        if (check == 1) {
-            ++xWinner;
-        } else if (check == 2) {
-            ++oWinner;
-        }
     }
-
     let mainDiagonal = checkButtons(buttons[0], buttons[4], buttons[8]);
     let secondaryDiagonal = checkButtons(buttons[2], buttons[4], buttons[6]);
-
-    if (mainDiagonal == 1 || secondaryDiagonal == 1) {
-        ++xWinner;
-    } else if (mainDiagonal == 2 || secondaryDiagonal == 2) {
-        ++oWinner;
-    }
-
-    if (xWinner) {
-        alert("Player 1 wins");
-        window.location.reload();
-    } else if (oWinner) {
-        alert("Player 2 wins");
-        window.location.reload();
-    }
 }
-
 
 function btn(button) {
     addValue(button);
